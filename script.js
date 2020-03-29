3d1bafee5c25a2081cef6a7a56073c83405a8870
muhdarifrawi <muhdarifrawi@gmail.com>
2020-03-29 21:15:03 +0800

    streamlined axios to fetch API and display on HTML.

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 /* global axios*/
 
-function testAPI(){
+function charClass(){
     axios.get(
-        "https://www.dnd5eapi.co/api/"
+        "https://www.dnd5eapi.co/api/"+"classes"
     ).then(function(response){
-        console.log(response.data)
+        let data = response.data.results[4]["name"]
+        console.log(data)
+        $(".class").html(data)
     })
+}
 
+
+function charProficiency(){
+    axios.get(
+        "https://www.dnd5eapi.co/api/"+"classes"+"/fighter"
+    ).then(function(response){
+        let data = response.data
+        console.log(data)
+
+    })
 }
